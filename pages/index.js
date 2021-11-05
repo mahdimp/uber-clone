90174853a1d546fe9361186cd3b861921453634d
Mahdi MP <m.maddahpour@yaama.tech>
2021-11-05 18:05:13 +0330

    Install Map Box

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import tw from 'tailwind-styled-components'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFoZGltcCIsImEiOiJjazJmdzdiZ24wbW12M3BvMnIyYWppbXNiIn0.sTFFue68x8aPsuekEiZLNg';
+
 
 export default function Home() {
+ 
+
+  useEffect(()=>{
+     const  map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: {lng :	59.599457, lat:	36.310699},
+    zoom:5
+    });
+  })
   return (
     <Wrapper>
-      <Map>Map</Map>
+      <Map id="map"></Map>
       <ActionItems>Start</ActionItems>
     </Wrapper>
   )
