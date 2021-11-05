30d85c038082a73d1dca8b09d7cd07decfae5a50
Mahdi MP <m.maddahpour@yaama.tech>
2021-11-05 17:37:44 +0330

    Add Tailwind support

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import tw from 'tailwind-styled-components'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.map}>Map</div>
-      <div className={styles.start}>Start</div>
-    </div>
+    <Wrapper>
+      <Map>Map</Map>
+      <ActionItems>Start</ActionItems>
+    </Wrapper>
   )
 }
+
+const Wrapper = tw.div`
+  flex flex-col bg-red-300 h-screen
+`
+
+const Map = tw.div`
+  bg-red-500 flex-1
+`
+
+const ActionItems = tw.div`
+flex-1
+`
