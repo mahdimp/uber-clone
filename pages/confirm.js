8706274047353e746c75a0b9e2ddd2dec6518f76
Mahdi MP <m.maddahpour@yaama.tech>
2021-11-06 15:22:22 +0330

    Inegrate Coordinates with Mapbox Api

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import tw from 'tailwind-styled-components/dist/tailwind'
 import Map from '../components/map';
+import { useEffect } from 'react';
 
 const Confirm = () => {
+
+    useEffect(() => {
+        getCoordinates()
+    }, [])
+
+    const getCoordinates = (name) => {
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${name}?` + new
+            URLSearchParams({
+                access_token: "pk.eyJ1IjoibWFoZGltcCIsImEiOiJjazJmdzdiZ24wbW12M3BvMnIyYWppbXNiIn0.sTFFue68x8aPsuekEiZLNg",
+                limit: 1
+            })).then(res => {
+                console.log(res)
+            })
+    }
+
     return (
         <Wrapper>
-            <Map/>
+            <Map />
         </Wrapper>
     )
 }
+
+
+
 
 export default Confirm
 
