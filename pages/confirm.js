0690f3d7845e9c65036581c4e0d87cbe47c1a5fa
Mahdi MP <m.maddahpour@yaama.tech>
2021-11-06 15:40:39 +0330

    Add State for Coordinates

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,23 +1,49 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components/dist/tailwind'
 import Map from '../components/map';
-import { useEffect } from 'react';
+
+const API_TOKEN = "pk.eyJ1IjoibWFoZGltcCIsImEiOiJjazJmdzdiZ24wbW12M3BvMnIyYWppbXNiIn0.sTFFue68x8aPsuekEiZLNg";
 
 const Confirm = () => {
 
-    useEffect(() => {
-        getCoordinates()
-    }, [])
+    const [pickOffCoordinates, setPickOffCoordinates] = useState()
+    const [dropOffCoordinates, setDropOffCoordinates] = useState()
 
-    const getCoordinates = (name) => {
-        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${name}?` + new
+     const getPickupCoordinates = (name) => {
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${name}.json?` + new
             URLSearchParams({
-                access_token: "pk.eyJ1IjoibWFoZGltcCIsImEiOiJjazJmdzdiZ24wbW12M3BvMnIyYWppbXNiIn0.sTFFue68x8aPsuekEiZLNg",
+                access_token: API_TOKEN,
                 limit: 1
-            })).then(res => {
-                console.log(res)
+            }))
+            .then(res => res.json())
+            .then(res => {
+                console.error(res)
+                if(res && res.features){
+                    const coordinate = res.features[0].center;
+                    pickOffCoordinates = coordinate;
+                }
             })
     }
+
+    const getDropOffCoordinates = (name) => {
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${name}.json?` + new
+            URLSearchParams({
+                access_token: API_TOKEN,
+                limit: 1
+            }))
+            .then(res => res.json())
+            .then(res => {
+                if(res && res.features){
+                    const coordinate = res.features[0].center;
+                    dropOffCoordinates = coordinate;
+                }
+            })
+    }
+
+    useEffect(() => {
+        getPickupCoordinates('Tehran')
+    }, [])
 
     return (
         <Wrapper>
