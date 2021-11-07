9c69a732c88617f07ba29e34a0728d5aae976010
Mahdi MP <m.maddahpour@yaama.tech>
2021-11-07 20:56:32 +0330

    Improve Map UI

--- constants/MapBox.js
+++ constants/MapBox.js
@@ -1,7 +1,7 @@
 const mapBoxApiToken = "pk.eyJ1IjoibWFoZGltcCIsImEiOiJjazJmdzdiZ24wbW12M3BvMnIyYWppbXNiIn0.sTFFue68x8aPsuekEiZLNg"
 const mapBoxDrivingApiUrl = "https://api.mapbox.com/directions/v5/mapbox/driving"
 const mapBoxPlacesApiUrl ="https://api.mapbox.com/geocoding/v5/mapbox.places"
-const mapBoxStyle = "mapbox://styles/mapbox/streets-v11";
+const mapBoxStyle = "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph";
 const mapBoxDefaultCenter = { lng: 59.599457, lat: 36.310699 };
 
 export {
